a4e20c2c63b58073c47ee70cb5db2f85703dbe4c
Priyanshi <priyanshianshu29@gmail.com>
2025-08-15 20:16:49 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign-up E-Library</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Create your E-Library Account</h2>
+    <form action="register.php" method="POST">
+        <label>Choose Username:</label>
+        <input type="text" name="username" placeholder="Choose username" required><br>
+        <label>Choose Password:</label>
+        <input type="password" name="password" placeholder="Choose password" required><br>
+        <button type="submit" name="register">Sign up</button>
+    </form>
+    <p>Already have an account? <a href="index.php">Login here</a></p>
+</body>
+</html>
