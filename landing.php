8d28e6c04664f030d485d44da489909bc9675f1a
Priyanshi <priyanshianshu29@gmail.com>
2025-08-16 15:39:40 +0530

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Library Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- header section -->
+    <header>
+        <div class="logo">📚 E-Library</div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#about">About Us</a>
+            <a href="#features">Features</a>
+            <a href="#contact">Contact Us</a>
+            <a href="index.php" class="login-button">Login</a>
+        </nav>
+    </header>
+
+    <!-- hero section -->
+    <section class="hero">
+        <h1>Welcome to the E-Library</h1>
+        <p>Reserve, search, and read books online from anywhere and anytime.</p>
+        <div class="search-bar">
+            <input type="text" placeholder="Search by title, author or category">
+            <button>Search</button>
+        </div>
+    </section>
+
+    <!-- Features section -->
+    <section id="features" class="features">
+        <h2>Key Features</h2>
+        <div class="feature-list">
+            <div class="feature">
+                <h3>Search books</h3>
+                <p>Find books by author, title, or category instantly.</p>
+            </div>
+            <div class="feature">
+                <h3>View Book Details</h3>
+                <p>Check author information, availability, and category for ech book.</p>
+            </div>
+            <div class="feature">
+                <h3>Reserve Books</h3>
+                <p>Reserve your online books with one click.</p> 
+            </div>
+        <div class="feature">
+            <h3>Admin Controls</h3>
+            <p>Separate login systems for users and administrators.</p>
+        </div>
+        <div class="feature">
+            <h3>View All Available Books</h3>
+            <p>Browse the complete list of available books in the library.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+     <section id="about" class="about">
+        <h2>About Us</h2>
+        <p>Our E-Library is a digital platform that provides easy access to a wide collection of books, journals, and study materials anytime, anywhere. We aim to promote learning by combining knowledge with technology for students, researchers, and readers.</p>
+     </section>
+
+     <!-- Contact Section -->
+      <section id="contact" class="contact">
+        <h2>Contact Us</h2>
+        <p>Email: booksupport@elibrary.com || Phone: +91 9898989898</p>
+      </section>
+
+      <!-- Footer -->
+       <footer>
+        <p>&copy; 2025 E-Library Management System || All Rights Reserved</p>
+       </footer>
+</body>
+</html>
